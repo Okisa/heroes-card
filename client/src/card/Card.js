72ad214754252a63c428f0e878b1a562fe8f6ca9
Isabela Mayumi Kawabata Borges <isakawa@br.ibm.com>
2017-08-20 04:30:06 -0300

    card

--- client/src/card/Card.js
+++ client/src/card/Card.js
@@ -3,9 +3,11 @@ import quizList from './quizList';
 import Quiz from './Quiz';
 import * as actions from './actions';
 import { connect } from 'react-redux';
+import {browserHistory} from 'react-router';
 import CardView from '../cardView/CardView';
 import './card.css';
 import axios from 'axios';
+import YourCard from '../images/template_card.jpg';
 
 let value;
 let fullAnswer=" ";
@@ -17,7 +19,14 @@ class Card extends Component {
         this.state={
             question:'',
             index:0,
-            quizIndex:0
+            quizIndex:0,
+            status1:'',
+            status2:'',
+            status3:'',
+            status4:'',
+            status5:'',
+            hero:'',
+            name:''
         };
 
         this.incrementIndex = this.incrementIndex.bind(this);
@@ -37,11 +46,21 @@ class Card extends Component {
         else{
             axios.get(`https://bluehack-heroes.mybluemix.net/api/heroes?name=Volverine&text=${this.fullAnswer}`)
                 .then(response => {
+                    localStorage.setItem('answer',response);
+                    this.setState({
+                        status1:Math.round(response.data[0].value),
+                        status2:Math.round(response.data[1].value),
+                        status3:Math.round(response.data[2].value),
+                        status4:Math.round(response.data[3].value),
+                        status5:Math.round(response.data[4].value),
+                        hero:response.data[5].name
+                    });
                     console.log(response);
                 })
                 .catch(error => {
                     console.log(error);
                 });
+            
         }
     }
 
@@ -62,7 +81,18 @@ class Card extends Component {
                             </div>
                         </div>
                         <div className="container__card">
-                            <CardView/>
+                            <div className="Status">
+                                <label className="PlayerName">Teste</label>
+                                <div className="StatusPlayer">
+                                    <label className="StatusOne">{this.state.status1}</label>
+                                    <label className="StatusTwo">{this.state.status2}</label>
+                                    <label className="StatusThree">{this.state.status3}</label>
+                                    <label className="StatusFour">{this.state.status4}</label>
+                                    <label className="StatusFive">{this.state.status5}</label>
+                                </div>
+                                <label className="YourHero">{this.state.hero}</label>
+                            </div>
+                            <img className="YourCard_right" src={YourCard} alt=""/>
                         </div>
                 </div>
             </div> 
